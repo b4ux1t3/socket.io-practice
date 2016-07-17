de256fa66dfc4f912a532c8a274593558dd54206
Chris P <flippedbauxite@gmail.com>
2016-07-17 18:37:20 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user has connected');
+  
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+});
+
+http.listen(8080, function(){
+  console.log('listening on *:8080');
+});
