243c7607f3c0aeb42dfee13ea53a140679d040a5
Chris P <flippedbauxite@gmail.com>
2016-07-17 16:01:20 -0700

    Implemented receiving messages

--- index.js
+++ index.js
@@ -10,13 +10,16 @@ io.on('connection', function(socket){
   console.log('a user has connected');
   
 
-  // socket.on('disconnect', function(){
-  //   console.log('user disconnected');
-  // });
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
+    io.emit('chat message', msg);
   })
+
+
 
 });
 
