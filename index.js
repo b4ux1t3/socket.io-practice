4d9cb7ab146a17a9147c1089d95499873875e5b8
Chris P <flippedbauxite@gmail.com>
2016-07-17 15:56:53 -0700

    Fixed one character bug

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ io.on('connection', function(socket){
   console.log('a user has connected');
   
 
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
+  // socket.on('disconnect', function(){
+  //   console.log('user disconnected');
+  // });
 
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
